589fd580ff601cff5f0589f955d16ceb6b41dcf1
Ujjayraj <ujjayraj5@gmail.com>
2025-11-06 23:05:44 +0800

    changes to modal,js and orders

--- frontend/admin/admin_dashboard.php
+++ frontend/admin/admin_dashboard.php
@@ -123,6 +123,7 @@
             </footer>
         </div>
     </div>
+    <?= include 'change&remove_modal.html'; ?>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
         crossorigin="anonymous"></script>
     <script src="../js/scripts.js"></script>
